7aaf8c678658cdbea84220cc4de358e40e2a3788
Takashi Bando <takashi@m-intuition.com>
2024-04-17 11:21:22 -0700

    Change repo name

--- Package.swift
+++ Package.swift
@@ -3,22 +3,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "asr",
+    name: "mispeech",
     platforms: [
         .iOS(.v16),
     ],
     products: [
         .library(
-            name: "asr",
-            targets: ["asr"]),
+            name: "mispeech",
+            targets: ["mispeech"]),
     ],
     dependencies: [
         .package(url: "https://github.com/dstbando/whisper.cpp", .upToNextMajor(from: "0.0.6")),
     ],
     targets: [
         .binaryTarget(
-            name: "asr",
-            url: "https://github.com/dstbando/asr/releases/download/0.0.4/asr.xcframework.zip",
+            name: "mispeech",
+            url: "https://github.com/dstbando/mispeech/releases/download/0.0.4/mispeech.xcframework.zip",
             checksum: "bed871d71325e4f11d35dd1eeaf5c7d0953243d3399ccd45f853e14df23c4705"),
     ]
 )
